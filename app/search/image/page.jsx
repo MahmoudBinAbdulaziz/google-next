c15abd667a8bc20e90a6374e949f7ac99342bdc2
Mahmoud <mahmoud.abdulazizz.1911@gmail.com>
2024-10-26 16:51:37 +0300

    finish

--- app/search/image/page.jsx
+++ app/search/image/page.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Link from "next/link";
+import ImageSearchResults from "@/app/components/imageSearchResults";
+export default async function Page(context) {
+  const { searchParams } = await context; // Await the context to access searchParams
+  const word = await searchParams;
 
-function Page() {
-  return <div>page</div>;
+  const searchTerm = word?.searchTerm || "";
+  const startIndex = word.start || "1";
+  let data;
+  if (!searchTerm) {
+    return <h1>No search term provided</h1>;
+  }
+  try {
+    const resp = await fetch(
+      `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}
+      &cx=${process.env.CONTEXT_KEY}&q=${searchTerm}&searchType=image&start=${startIndex}`
+    );
+
+    data = await resp.json();
+    // console.log(data);
+  } catch (error) {}
+  const results = data?.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found for {searchTerm}</h1>
+        <p className="text-lg">
+          Try searching the web or images for something else{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+  return <div>{results && <ImageSearchResults results={data} />}</div>;
 }
-
-export default Page;
